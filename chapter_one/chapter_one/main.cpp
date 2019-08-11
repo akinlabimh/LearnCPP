5a8dc730b843eb37d873c46383f3a23ebd664f9e
Akin Mason-Hogans <admasonhogans@aggies.ncat.edu>
2019-08-11 02:32:03 -0400

    chapter1

---
+++ chapter_one/chapter_one/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  chapter_one
+//
+//  Created by Akin Mason-Hogans on 8/11/19.
+//  Copyright © 2019 akin. All rights reserved.
+//
+
+#include <iostream> // Import the input-output library
+using namespace std; //allows us to write cout instead of std::cout
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "I'm learning C++!" << endl;
+    return 0;
+}
